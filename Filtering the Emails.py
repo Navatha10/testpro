67ca9a33c154fdec4c73df9bb4ea93ae3b7eb775
Navatha10 <141243321+Navatha10@users.noreply.github.com>
2023-08-09 13:55:59 +1000

    Add files via upload

---
+++ Filtering the Emails.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+Filtering the Emails
+
+
+# In[ ]:
+
+
+Some mails are offering us deals or discounts. Some mails are asking passwords or personal information. All these mails are
+looking fishy and spam .To separate these mails from the inbox.
+
+
+# In[ ]:
+
+
+The Question for this project is "Can we separate these unwanted mails from the inbox ?"
+
+
+# In[ ]:
+
+
+1 . Analytic Approach
+2 . Data Requirements
+3 . Data Collection
+4 . Data Understanding and Preparation
+5 . Modeling and Evaluation
+
+
+# In[ ]:
+
+
+1 . Analytic Approach
+
+Once the business problem has been clearly stated, the data scientist can define the analytic approach. 
+Expressing the problem in the context of statistical and machine-learning techniques,so the data scientist
+can identify the most suitable techniques for the desired outcome.
+The problem requires a Inbox/Spam answer so we use classification model
+
+
+# In[ ]:
+
+
+2 . Data Requirements
+
+Data requirements choice of analytic approach determines the data requirements,for the analytic methods 
+to be used require certain data content, formats and representations.
+
+
+# In[ ]:
+
+
+3 . Data collection 
+
+The data scientist identifies and gathers data resources- structured and unstructured data that are relevant to data.
+The data scientist might need to revise the data requirements after encountering the gaps and collect more data if required.
+
+
+# In[ ]:
+
+
+4 . Data Understanding and Preparation
+
+We use descriptive statistics and data evaluation techniques can understand data content, access data quality and discover 
+initial insights into the data.
+In the data preparation stage all activities used to construct the data set that will be used in the Modeling stage.
+Data cleaning, merging data from variable sources and transforming data into variables.
+Feature engineering and text analytics may be used to derive new structured variables to enhance the accuracy model.
+Data preparation stage is the most time consuming.
+
+
+# In[ ]:
+
+
+5 . Modeling and Evaluation
+
+Data scientists use a training set with the first prepared data. The historical data in which outcome is known.
+Using the analytic approach we can develop the predictive or descriptive models. And the modeling process is highly iterative.
+In the evaluation stage the data scientist evaluates the models quality. For this quality check we requires the computing 
+of various diagnostic measures such as tables and graphs.
+
